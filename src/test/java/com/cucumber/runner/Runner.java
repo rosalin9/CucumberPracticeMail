0a0b767a8321d98478543eb2d7c827375ca08bbd
rosalin9 <83919220+rosalin9@users.noreply.github.com>
2024-03-19 23:31:16 -0700

    Update Runner.java
    update runner file

--- src/test/java/com/cucumber/runner/Runner.java
+++ src/test/java/com/cucumber/runner/Runner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="C:\\Users\\rosal\\eclipse-workspace\\CucumberBasic\\features\\LoginScenarioOutline.feature",
-					glue = {"com.cucumber.commonsteps"},
+					glue = {"com.cucumber.steps"},
 					plugin = {"pretty", "html:target/cucumber-reports/cucumber.html", 
 										"json:target/cucumber-reports/cucumber.json"}			
 		)
